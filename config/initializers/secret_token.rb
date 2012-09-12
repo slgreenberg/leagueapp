e6ea0943c6f23fe08ef366af213677663ba43c77
Susan Greenberg <susangr@seas.upenn.edu>
2012-09-12 18:23:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leagueapp::Application.config.secret_token = '5316c87f814729b3570b5a915205434ebef79229d1f49ab8d0cbd5df3ac7897f3321dcfa654fe39a572dd0871a70223bc82a1335d7dd13a02697bf1cb8d352d7'
